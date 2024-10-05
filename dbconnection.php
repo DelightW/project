ea64d5a41700f6f251665b46e56d11c1f6456cae
DelightW <148865109+DelightW@users.noreply.github.com>
2024-10-05 21:13:25 +0300

    ...

---
+++ dbconnection.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = " ";
+    private $dbname = "PROJECT";
+    public $conn;
+
+    // Get the database connection
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        } catch (Exception $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+
+// Usage
+$database = new Database();
+$db = $database->getConnection();
+?>
