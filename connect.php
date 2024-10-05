da920253662633c9ce8981004f746e77c1de601a
DelightW <148865109+DelightW@users.noreply.github.com>
2024-10-05 19:08:32 +0300

    continue

--- connect.php
+++ connect.php
@@ -16,7 +16,7 @@
 		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
 		$execval = $stmt->execute();
 		echo $execval;
-		echo "Registration successfully...";
+		echo "Successful registration";
 		$stmt->close();
 		$conn->close();
 	}
